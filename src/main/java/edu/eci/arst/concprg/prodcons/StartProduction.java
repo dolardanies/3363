680e8228e48de5f0f6d13b8a55d26ff07349e1b9
d.castiblanco <dolardanies@hotmail.com>
2018-08-22 12:57:42 -0500

    Parte 1_2

--- src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
@@ -16,25 +16,22 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class StartProduction {
-    
-    
+
     public static void main(String[] args) {
-        
-        Queue<Integer> queue=new LinkedBlockingQueue<>();
-        
-        
-        new Producer(queue,Long.MAX_VALUE).start();
-        
+
+        Queue<Integer> queue = new LinkedBlockingQueue<>();
+
+        new Producer(queue, Long.MAX_VALUE).start();
+
         //let the producer create products for 5 seconds (stock).
         try {
             Thread.sleep(5000);
         } catch (InterruptedException ex) {
             Logger.getLogger(StartProduction.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
+
         new Consumer(queue).start();
+        
     }
-    
 
 }
